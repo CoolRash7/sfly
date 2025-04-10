82f9c68f32382ba95bdcd99b22c4cd202ff32aa7
CoolRash <coolrash@gmail.com>
2025-04-10 11:53:59 +0900

    first commit

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function serviceTemp(int $id) {   
         $temp = Service::where('id', $id)->first();
-        $username = 'Стрей227';
+        $username = User::where('id', $temp->user_id) ?? 'Пользователь удален';
         return view('service', compact('temp', 'username'));
     }
 
